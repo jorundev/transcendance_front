315bdd661d06bb944327b983a95293c24d3534e9
Jorun <contact@jorun.dev>
2022-11-29 15:58:39 +0100

    Added game overlay

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
 	import { pop, replace } from "svelte-spa-router";
 	import { GAME_HEIGHT, GAME_WIDTH } from "../pong/Pong";
-	import { onDestroy } from "svelte";
+	import { onDestroy, onMount } from "svelte";
 	import type { PongClient } from "../pong/PongClient";
 	import { stGameSettings, stLobby, stPongClient } from "../stores";
-	import { api } from "../api";
+	import type { User } from "../users";
+	import { api, APIStatus } from "../api";
+	import { padIdentifier } from "../utils";
+	import UserAvatar from "../components/Users/UserAvatar.svelte";
 
 	let canvas: HTMLCanvasElement;
 	let game: HTMLDivElement;
 	let ctx: CanvasRenderingContext2D;
 	let pong: PongClient = null;
+
+	let player1: User;
+	let player2: User;
+
+	onMount(async () => {
+		const p1 = await api.getUserData($stLobby?.players[0]);
+		const p2 = await api.getUserData($stLobby?.players[1]);
+
+		if (p1 !== null && p1 !== APIStatus.NoResponse) {
+			player1 = p1;
+		}
+		if (p2 !== null && p2 !== APIStatus.NoResponse) {
+			player2 = p2;
+		}
+	});
+
+	let displayOverlay = false;
 
 	let secs = 0;
 	let oldTs = 0;
@@ -24,7 +44,6 @@
 
 	$: if (!$stPongClient) {
 		if ($stLobby) {
-			console.log($stLobby);
 			replace("/play/casual");
 		} else {
 			pop();
@@ -117,7 +136,7 @@
 		ctx = canvas.getContext("2d");
 		pong = $stPongClient;
 		if (pong) {
-			pong.setColors($stGameSettings.colors ?? ['white', 'white']);
+			pong.setColors($stGameSettings.colors ?? ["white", "white"]);
 		}
 		play = true;
 		window.requestAnimationFrame(gameLoop);
@@ -174,6 +193,52 @@
 		class:red={$stGameSettings.background === "red"}
 		class:green={$stGameSettings.background === "green"}
 	>
+		<div
+			class="overlay"
+			class:active={displayOverlay}
+			class:blur={displayOverlay}
+		>
+			{#if !displayOverlay}
+				<div
+					class="display-overlay-button"
+					on:click={() => (displayOverlay = true)}
+				>
+					Menu
+				</div>
+			{:else}
+				<div class="layout">
+					<div class="players">
+						<div class="player">
+							<div class="avatar">
+								<UserAvatar uuid={player1?.uuid}></UserAvatar>
+							</div>
+							<div class="info">
+								{player1?.username}<div class="id">#{padIdentifier(parseInt(player1?.identifier))}</div>
+							</div>
+						</div>
+						<div class="player">
+							<div class="avatar">
+								<UserAvatar uuid={player2?.uuid}></UserAvatar>
+							</div>
+							<div class="info">
+								{player2?.username}<div class="id">#{padIdentifier(parseInt(player2?.identifier))}</div>
+							</div>
+						</div>
+					</div>
+					<div class="options">
+						<div
+							class="leave-overlay"
+							on:click={() => (displayOverlay = false)}
+						>
+							Back to game
+						</div>
+						<div class="leave-game" on:click={() => replace("/")}>
+							Leave game
+						</div>
+					</div>
+				</div>
+			{/if}
+		</div>
 		<canvas
 			class="game-canvas"
 			bind:this={canvas}
@@ -194,6 +259,7 @@
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
+		align-items: center;
 		width: 100%;
 		height: 100%;
 		padding: 20px;
@@ -213,6 +279,108 @@
 				left: 0;
 				top: 0;
 				z-index: 0;
+			}
+
+			.overlay {
+				//pointer-events: none;
+				position: absolute;
+				z-index: 1000;
+
+				bottom: 0;
+				left: 50%;
+
+				&.active {
+					left: 0;
+					top: 0;
+					width: 100%;
+					height: 100%;
+				}
+
+				&.blur {
+					background-color: rgba(0, 0, 0, 0.464);
+					backdrop-filter: blur(6px);
+				}
+
+				.layout {
+					position: relative;
+					width: 100%;
+					height: 100%;
+
+					display: grid;
+					place-items: center;
+
+					.options {
+						margin: 20px;
+
+						width: 100%;
+						max-width: 240px;
+						> * {
+							padding: 10px;
+							border-bottom: 1px solid rgba(255, 255, 255, 0.212);
+							background: rgba(0, 0, 0, 0.445);
+							backdrop-filter: blur(6px);
+							user-select: none;
+							-webkit-user-select: none;
+
+							&:hover {
+								cursor: pointer;
+								transform: scale(1.1);
+							}
+						}
+					}
+					
+					.avatar {
+						padding-right: 10px;
+						width: 40px;
+						height: 40px;
+						flex-shrink: 0;
+					}
+
+					.players {
+						display: flex;
+						justify-content: space-between;
+						position: absolute;
+						top: 0;
+						height: 60px;
+						width: 100%;
+						&:nth-child(1) {
+							flex-direction: row;
+						}
+						&:nth-child(2) {
+							flex-direction: row-reverse;
+						}
+
+						> * {
+							display: flex;
+							align-items: center;
+							padding-left: 20px;
+							padding-right: 20px;
+							user-select: none;
+
+							background: rgba(0, 0, 0, 0.445);
+						}
+					}
+				}
+				
+				.info {
+					display: flex;
+					.id {
+						color: rgba(255, 255, 255, 0.459);
+					}
+				}
+
+				.display-overlay-button {
+					cursor: pointer;
+					border: max(1px, 0.1vw) solid white;
+					background: rgba(0, 0, 0, 0.445);
+					position: absolute;
+					backdrop-filter: blur(6px);
+					left: 50%;
+					bottom: 4px;
+					transform: translateX(-50%);
+					padding: min(2vw, 20px);
+					font-size: min(2vw, 20px);
+				}
 			}
 
 			&.blue::before {
